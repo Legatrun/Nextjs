0962c27c4389170249669e74f45f72dc55dfb554
legatrun <joaquimmerlo1@gmail.com>
2021-10-07 16:33:28 -0400

    subiendo el proyecto

---
+++ components/layout.jsx
@@ -0,0 +1,42 @@
+import NavBar from "./NavBar";
+
+export default function Layout({children}) {
+    return (
+        <div>
+            <NavBar/>
+            <main>
+                {children}
+            </main>
+            {/* para que los estilos seran globales le añadimos el "global" a nuestro style */}
+            <style jsx>
+                {`
+                    div{
+                        min-height: 100vh;
+                        display:flex;
+                        flex-direction:column;
+                        justify-content:center;
+                        align-items:center;
+                    }
+                    main{
+                        flex:1;
+                        display:flex;
+                        flex-direction:column;
+                        align-items:center;
+                    }
+                `}
+            </style>
+            <style jsx global> 
+                {`
+                    html,
+                    body{
+                        padding:0;
+                        margin:0;
+                    }
+                    *{
+                        box-sizing: border-box;
+                    }
+                `}
+            </style>
+        </div>
+    )
+}
---
+++ components/title.jsx
@@ -0,0 +1,16 @@
+export default function Title({children}) {
+    return (
+        <div>
+            <h1>{children}</h1>
+            <style jsx>
+                {`
+                    h1{
+                        margin:0;
+                        font-size:3rem;
+                        text-align:center;
+                    }
+                `}
+            </style>
+        </div>
+    )
+}
